606403d51bffa5f9a4257d50a30db90b163f6dd8
zanecosmo <zanecosmo@gmail.com>
2022-04-07 21:00:04 -0400

    freeform string reversal

--- test-script.js
+++ test-script.js
@@ -1,1 +1,11 @@
-console.log("TESTING ONE TWO THREE");
+const reverseString = (string) => {
+    let newString = "";
+
+    for (let i = string.length-1; i >= 0; i--) {
+        newString += string[i];
+    };
+
+    console.log(newString)
+};
+
+reverseString("FART");
